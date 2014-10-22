a950a6127f113b748a1560a10235331e8412099c
JosCspds <jdc8@njit.edu>
2014-10-22 15:29:46 -0400

    Recognize function added

--- Assets/Scripts/Vognition.cs
+++ Assets/Scripts/Vognition.cs
@@ -10,13 +10,83 @@ public class Vognition : MonoBehaviour
 {
 
     string postScoreURL = "http://sample.whataremindsfor.com:46900/apiv1/transtext";
-    
-	public void TransText()
-	{
-		string response = vogTransText("Raise the degrees", "usenglishmale", "en-US");
-		Debug.Log (response);
-	}
+	string postScoreURL2 = "http://sample.whataremindsfor.com:46900/apiv1/recognize";
 	
+	public void Recognize()
+	{
+		string response = vogSpeechRecognition(Path.GetFileName("Audio/RaiseTheDegressBy5.wav"), "usenglishmale", "en-US");
+		Debug.Log(response);
+	}
+
+    public void TransText()
+    {
+        string response = vogTransText("Raise the degrees by 5", "usenglishmale", "en-US");
+        Debug.Log(response);
+    }
+
+	public string vogSpeechRecognition(String wav, String ttsSpeakerType, String locale)
+	{
+		string retVal = "_NULL";
+		using (var wb = new WebClient())
+		{
+			var data = new NameValueCollection();
+			//  **CAPSTONE KEYS**
+			data["appkey"] = "59cddd076cf2a00b9f5555585c177276d8a49873";
+			data["appsecret"] = "01d3cb9455722c2f6760e11f14a0571179649873";
+			data["conkey"] = "09326eefff2cad2ae9f4c0fd5d4c7b71db249873";
+			data["consecret"] = "7092379b31cd621c2c1b574e47ec956733f49873";
+			data["wav"] = wav;
+			data["ttsSpeakerType"] = ttsSpeakerType;
+			data["locale"] = locale;
+			data["requestType"] = "ASR";
+			data["HPDL_ID"] = "TEST_HPM_ID";
+			try
+			{
+				var response = wb.UploadValues(postScoreURL2, "POST", data);
+				retVal = Encoding.UTF8.GetString(response, 0, response.Length);
+			}
+			catch (WebException e)
+			{
+				int statusCode = (int)((HttpWebResponse)e.Response).StatusCode;
+				switch (statusCode)
+				{
+				case 400:
+					retVal = "Response code: " + statusCode + "\tBad request";
+					break;
+					
+				case 401:
+					retVal = "Response code: " + statusCode + "\tUnauthorized authentication";
+					break;
+					
+				case 403:
+					retVal = "Response code: " + statusCode + "\tResource is not availible";
+					break;
+					
+				case 409:
+					retVal = "Response code: " + statusCode + "\tMissing Parameters";
+					break;
+					
+				case 501:
+					retVal = "Response code: " + statusCode + "\tFeature not yet implemented";
+					break;
+					
+				case 503:
+					retVal = "Response code: " + statusCode + "\tThere is an issue on Vognition";
+					break;
+					
+				case 504:
+					retVal = "Response code: " + statusCode + "\tComponent Time Out";
+					break;
+					
+				default:
+					return ((HttpWebResponse)e.Response).StatusDescription;
+				}
+			}
+			catch (Exception e) { Console.WriteLine(e.Message); }
+		}
+		return retVal;
+	}
+
     public string vogTransText(String sentence, String ttsSpeakerType, String locale)
     {
         string retVal = "_NULL";
@@ -28,7 +98,7 @@ public class Vognition : MonoBehaviour
             data["appsecret"] = "01d3cb9455722c2f6760e11f14a0571179649873";
             data["conkey"] = "09326eefff2cad2ae9f4c0fd5d4c7b71db249873";
             data["consecret"] = "7092379b31cd621c2c1b574e47ec956733f49873";
-            
+
             //  **VOGNITION HACKATHON KEYS
             //data["appkey"] = "59c000476cf2a00b9f5bc1585c177276d8a4d75f";
             //data["appsecret"] = "01d3cb9455722c2f6760e11f14a05711796abc1a";
@@ -36,7 +106,6 @@ public class Vognition : MonoBehaviour
             //data["consecret"] = "7092379b31cd621c2c1b574e47ec956733fabc1a";
 
             data["sentence"] = sentence;
-            //data["sentence"] = "C:\\Users\\Richard\\Desktop\\sample.wma";
             data["ttsSpeakerType"] = ttsSpeakerType;
             data["locale"] = locale;
             data["requestType"] = "ASR";
@@ -48,38 +117,43 @@ public class Vognition : MonoBehaviour
             }
             catch (WebException e)
             {
-                int statusCode = (int) ((HttpWebResponse)e.Response).StatusCode;
+                int statusCode = (int)((HttpWebResponse)e.Response).StatusCode;
                 switch (statusCode)
                 {
                     case 400:
-                        return "Response code: " + statusCode + "\tBad request";
+                        retVal = "Response code: " + statusCode + "\tBad request";
+                        break;
 
                     case 401:
-                        return "Response code: " + statusCode + "\tUnauthorized authentication";
+                        retVal = "Response code: " + statusCode + "\tUnauthorized authentication";
+                        break;
 
                     case 403:
-                        return "Response code: " + statusCode + "\tResource is not availible";
+                        retVal = "Response code: " + statusCode + "\tResource is not availible";
+                        break;
 
                     case 409:
-                        return "Response code: " + statusCode + "\tMissing Parameters";
+                        retVal = "Response code: " + statusCode + "\tMissing Parameters";
+                        break;
 
                     case 501:
-                        return "Response code: " + statusCode + "\tFeature not yet implemented";
-                        
+                        retVal = "Response code: " + statusCode + "\tFeature not yet implemented";
+                        break;
+
                     case 503:
-                        return "Response code: " + statusCode + "\tThere is an issue on Vognition";
+                        retVal = "Response code: " + statusCode + "\tThere is an issue on Vognition";
+                        break;
 
                     case 504:
-                        return "Response code: " + statusCode + "\tComponent Time Out";
+                        retVal = "Response code: " + statusCode + "\tComponent Time Out";
+                        break;
 
                     default:
                         return ((HttpWebResponse)e.Response).StatusDescription;
                 }
             }
-            catch (Exception e) { Console.WriteLine(e.Message); }    
+            catch (Exception e) { Console.WriteLine(e.Message); }
         }
         return retVal;
     }
 }
-
-
